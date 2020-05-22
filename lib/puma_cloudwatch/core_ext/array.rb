ddd29decb2d30b16459da08d7ec102b3e1557823
Ed Ruder <ed@aptopayments.com>
2020-05-22 11:00:23 -0700

    Eliminate the need to add Array#sum

--- lib/puma_cloudwatch/core_ext/array.rb
+++
@@ -1,7 +0,0 @@
-class Array
-  # Note: Array#sum only available in ruby 2.4+
-  # Thanks: http://www.viarails.net/q/How-to-sum-an-array-of-numbers-in-Ruby
-  def sum
-    inject(0) {|sum, i|  sum + i }
-  end
-end
